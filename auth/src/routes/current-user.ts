ba83339e6309cacc66d3f5de05fe01a98bedc64f
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2022-01-21 18:44:41 +0100

    Added route authentication and current user checking via middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,14 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { currentUser } from "../middleware/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  console.log("hello");
-});
+router.get(
+  "/api/users/currentuser",
+  currentUser,
+  (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
+  }
+);
 
 export { router as currentUserRouter };
