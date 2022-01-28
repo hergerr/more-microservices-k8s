ac607b6032dd13e8c98afe3f39e101508b0d3e38
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2022-01-28 20:53:49 +0100

    Added getInitialProps in custom app and lacking auth pages

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,35 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+import Header from "../components/header";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />;
+    </div>
+  );
 };
+
+// diffrent args than in normal Page
+// getting information about current use here
+// makes it being passed to child components
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+
+  if (appContext.Component.getInitialProps) {
+    // calling getInitialProps for Pages
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    // currentUser: data.currentUser,
+    ...data,
+  };
+};
+
+export default AppComponent;
