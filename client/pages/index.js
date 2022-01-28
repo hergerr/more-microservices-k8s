808b7476ca29d8a7b3057b0caf51b46f4a459780
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2022-01-28 17:52:44 +0100

    Handled making server and browser based requests

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,29 @@
-export default () => {
-  return <h1>Lalas</h1>;
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
+  return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    // on server
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        // proxying headers, but most importantly
+        // host - to know domain, to which request is made (ingress needs it)
+        // cookie - for auth
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    // browser
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+};
+
+export default LandingPage;
