9a82d931aa89e6c5f45676b2a48ed9e67a923f31
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2022-02-10 15:43:51 +0100

    Publishing saving and updating events

---
+++ tickets/src/nats-wrapper.ts
@@ -0,0 +1,32 @@
+import nats, { Stan } from "node-nats-streaming";
+
+// mongoose-like wrapper singleton class
+class NatsWrapper {
+  // only declaration
+  private _client?: Stan;
+
+  get client() {
+    if (!this._client) {
+      throw new Error("Cannot access NATS client before connecting");
+    }
+
+    return this._client;
+  }
+
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise<void>((resolve, reject) => {
+      this.client.on("connect", () => {
+        console.log("Connected to NATS");
+        resolve();
+      });
+      this.client.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+}
+
+// sharing one instance across multiple files
+export const natsWrapper = new NatsWrapper();
