72a2136a00692e84daf5eabda2cc446b7d345636
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2022-02-18 16:44:21 +0100

    Added base payments setup

---
+++ payments/src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@tftickects/common";
+
+const app = express();
+// stuff is proxied through ingress. Trust it
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    // cookie only used in https connection (in prod)
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+app.use(currentUser);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+// middleware
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,65 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose, { ConnectOptions } from "mongoose";
+import jwt from "jsonwebtoken";
+
+declare global {
+  var signin: () => string[];
+}
+
+jest.mock("../nats-wrapper");
+
+let mongo: MongoMemoryServer;
+// run before all tests
+beforeAll(async () => {
+  // clear mock functions statistict
+  jest.clearAllMocks();
+
+  process.env.JWT_KEY = "asdadas";
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as ConnectOptions);
+});
+
+// before each test
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+// after all tests
+afterAll(async () => {
+  // other way causes memory leaks
+  await mongoose.connection.close();
+  await mongo.stop();
+});
+
+global.signin = () => {
+  // build a JWT payload. {id, email}
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
+  };
+
+  // create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // build session object
+  const session = { jwt: token };
+
+  // turn session into json
+  const sessionJSON = JSON.stringify(session);
+
+  // encode json as base64 (like cookie-session package)
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // return cookie with encoded data (supertests expects them in an array)
+  return [`session=${base64}==`];
+};
