53c3493022b75ef7c80f84d21f556dd42d6fea9d
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2022-02-21 16:20:17 +0100

    Connected listeners

---
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,29 @@
+import {
+  OrderCancelledEvent,
+  Subjects,
+  Listener,
+  OrderStatus,
+} from "@tftickects/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+  queueGroupName: string = queueGroupName;
+  async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+    const order = await Order.findOne({
+      _id: data.id,
+      // idk why 449
+      version: data.version - 1,
+    });
+
+    if (!order) {
+      throw new Error("Order not found");
+    }
+
+    order.set({ status: OrderStatus.Cancelled });
+    await order.save();
+    msg.ack();
+  }
+}
