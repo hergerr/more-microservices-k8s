d9ec377d5e39df7e55cf19c53021d2e7ceadb88b
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2022-02-23 15:18:52 +0100

    Added payments model

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import { app } from "../../app";
 import { Order } from "../../models/order";
 import { OrderStatus } from "@tftickects/common";
 import { stripe } from "../../stripe";
+import { Payment } from "../../models/payment";
 
 jest.mock("../../stripe");
 
@@ -84,7 +85,17 @@ it("returns a 204 with valid inputs", async () => {
     .expect(201);
 
   const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+  const chargeResult = await (stripe.charges.create as jest.Mock).mock
+    .results[0].value;
+
   expect(chargeOptions.source).toEqual("tok_visa");
   expect(chargeOptions.amount).toEqual(20 * 100);
   expect(chargeOptions.currency).toEqual("usd");
+
+  const payment = await Payment.findOne({
+    orderId: order.id,
+    stripeId: chargeResult.id,
+  });
+
+  expect(payment).not.toBeNull();
 });
